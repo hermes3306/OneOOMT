934d3ba2b86ba5d22da9be0770cd0b3463142369
hermes3306 <joonho.park@hotmail.com>
2017-12-16 02:27:36 +0900

    일본방문전에 버그 피스함

--- runme/src/main/java/com/joonho/runme/MapsActivity.java
+++ runme/src/main/java/com/joonho/runme/MapsActivity.java
@@ -1,22 +1,38 @@
 package com.joonho.runme;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.joonho.runme.util.ActivityUtil;
+import com.joonho.runme.util.CalDistance;
 import com.joonho.runme.util.MyActivity;
+import com.joonho.runme.util.MyNotifier;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener  {
@@ -78,23 +94,140 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    public static void drawTrack(GoogleMap gmap, ArrayList<MyActivity> list) {
+        ArrayList<LatLng> l = new ArrayList<>();
+        for(int i=0; i<list.size();i++) {
+            l.add(new LatLng(list.get(i).latitude, list.get(i).longitude));
+        }
+
+        PolylineOptions plo = new PolylineOptions();
+        plo.color(Color.RED);
+        Polyline line = gmap.addPolyline(plo);
+        line.setWidth(20);
+        line.setPoints(l);
+    }
+
+    public double getTotalDistanceDouble(ArrayList<MyActivity> list) {
+        if(list == null) return 0;
+        if(list.size() ==2) return 0;
+
+        double dist_meter = 0;
+        for(int i=0; i<list.size()-1; i++) {
+            double bef_lat = list.get(i).latitude;
+            double bef_lon = list.get(i).longitude;
+            double aft_lat = list.get(i+1).latitude;
+            double aft_lon = list.get(i+1).longitude;
+
+            CalDistance cd = new CalDistance(bef_lat, bef_lon, aft_lat, aft_lon);
+            double dist_2 = cd.getDistance();
+            if(Double.isNaN(dist_2)) {
+                Log.e(TAG, "Double.NaN between ("+bef_lat + ","+ bef_lon +") ~ ("+ aft_lat + ","+ aft_lon + ")" ) ;
+                continue;
+            } else if ( Double.isNaN(dist_meter + dist_2)) {
+                Log.e(TAG, "Double.NaN between ("+bef_lat + ","+ bef_lon +") ~ ("+ aft_lat + ","+ aft_lon + ")" ) ;
+                continue;
+            }
+            dist_meter = dist_meter + dist_2;
+            //Log.e(TAG, "" + i + "]" +  list.get(i).added_on + dist_2 + " sum: " + dist_meter +  " ("+bef_lat + ","+ bef_lon +") ~ ("+ aft_lat + ","+ aft_lon + ")");
+            //Log.e(TAG, "" + dist_2 + " sum: " + dist_meter);
+        }
+        return dist_meter;
+    }
+
+    public ArrayList<Marker> markers = new ArrayList<Marker>();
+
+    public void drawMarkers(GoogleMap gmap, ArrayList<MyActivity> list) {
+        double tot_distance = getTotalDistanceDouble(list);
+
+        int disunit = 1000;
+        String unitstr = "미터";
+        if (tot_distance > 1000) {  // 1km 이상
+            disunit = 1000;
+            unitstr = "킬로";
+        } else disunit = 100;
+
+        double t_distance = 0;
+        double t_lap = disunit;
+        for(int i=0; i < list.size(); i++) {
+            LatLng ll = new LatLng(list.get(i).latitude, list.get(i).longitude);
+            float color = (i==0) ?  BitmapDescriptorFactory.HUE_GREEN : ((i==list.size()-1)? BitmapDescriptorFactory.HUE_RED  :  BitmapDescriptorFactory.HUE_CYAN);
+
+            String title = list.get(i).added_on;
+            if(i==0) {
+                Marker marker = gmap.addMarker(new MarkerOptions().position(ll).title(title)
+                        .icon(BitmapDescriptorFactory.defaultMarker(color))
+                        .draggable(true)
+                        .visible(true)
+                        .snippet("출발"));
+                markers.add(marker);
+            } else if(i==list.size()-1) {
+                Marker marker = gmap.addMarker(new MarkerOptions().position(ll).title(title)
+                        .icon(BitmapDescriptorFactory.defaultMarker(color))
+                        .draggable(true)
+                        .visible(true)
+                        .snippet("종료"));
+                markers.add(marker);
+            }
+            else {
+                CalDistance cd = new CalDistance(list.get(i-1).latitude, list.get(i-1).longitude, list.get(i).latitude, list.get(i).longitude);
+                double dist = cd.getDistance();
+                if(Double.isNaN(dist)) continue;
+                if(Double.isNaN(dist + t_distance)) continue;
+
+                t_distance = t_distance + dist;
+                if(t_distance > t_lap) {
+                    int interval = (int)(t_distance / disunit);
+                    //Log.e(TAG, "" + interval + unitstr);
+                    t_lap += disunit;
+
+                    Marker marker = gmap.addMarker(new MarkerOptions().position(ll).title(title)
+                            .icon(BitmapDescriptorFactory.defaultMarker(color))
+                            .draggable(true)
+                            .visible(true)
+                            .snippet(""+interval + unitstr));
+                    markers.add(marker);
+                }
+            }
+        }
+    }
+
+    public void doBoundBuild(GoogleMap gmap, int width, int height) {
+        if(markers.size()==0) return;
+
+        LatLngBounds.Builder builder= new LatLngBounds.Builder();
+        for (Marker marker : markers) {
+            builder.include(marker.getPosition());
+        }
+        LatLngBounds bounds = builder.build();
+        int padding = (int) (width * 0.10); // offset from edges of the map 10% of screen
+
+        boolean berr = false;
+        try {
+            Log.e(TAG, "newLatLngBounds(bounds):" + bounds);
+            Log.e(TAG, "newLatLngBounds(padding):" + padding);
+
+            CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+            gmap.moveCamera(cu);
+        }catch(Exception e) {
+            berr = true;
+            Log.e(TAG,"ERR] BoundBuild:" + e.toString());
+        }
+    }
 
     public void drawGoogleMap(ArrayList<MyActivity> mList, GoogleMap gmap, int width, int height, boolean mode_append) {
 
-        ActivityUtil.drawTrack(gmap, mList);
+        drawTrack(gmap, mList);
         Log.e(TAG, "ActivityUtil.drawTrack();");
 
-        ActivityUtil.drawMarkers(gmap, mList);
+        drawMarkers(gmap, mList);
         Log.e(TAG, "ActivityUtil.drawMarkers();");
 
-        ActivityUtil.doBoundBuild(gmap, width, height);
+        doBoundBuild(gmap, width, height);
         Log.e(TAG, "ActivityUtil.doBoundBuild();");
 
         LatLng ll = new LatLng( mList.get(mList.size()-1).latitude, mList.get(mList.size()-1).longitude);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 14));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, 15));
     }
-
-
 
 
 }
